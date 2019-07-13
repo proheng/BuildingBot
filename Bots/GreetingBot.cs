5a4bce154ff72d04d81ebf4f2f06342d77f82d3d
Rex Zhiheng He <rex@rex-he.net>
2019-07-13 21:52:08 +1000

    initial commit

---
+++ Bots/GreetingBot.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EchoBot1.Bots
+{
+    public class GreetingBot : ActivityHandler
+    {
+        protected override Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            return base.OnMessageActivityAsync(turnContext, cancellationToken);
+        }
+
+        protected override Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            foreach(var member in membersAdded)
+            {
+                if(member.Id != turnContext.Activity.Recipient.Id)
+                {
+
+                }
+            }
+            return base.OnMembersAddedAsync(membersAdded, turnContext, cancellationToken);
+        }
+    }
+}
---
+++ Services/BotStateService.cs
@@ -0,0 +1,23 @@
+﻿using EchoBot1.Models;
+using Microsoft.Bot.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EchoBot1.Services
+{
+    public class BotStateService
+    {
+        #region Variables
+        // State Variable
+        public UserState UserState { get; }
+
+        // IDs
+        public static string UserProfileId { get; } = $"{nameof(BotStateService)}.UserProfile";
+
+        // Accessors
+        public IStatePropertyAccessor<UserProfile> UserProfileAccessor { get; set; }
+        #endregion
+    }
+}
