ca492fb9f99e4a0b4b24a6f470302d10e58eb4a2
Rex Zhiheng He <zhiheng.he@pmc.gov.au>
2019-07-15 17:03:17 +1000

    Add bug report dialog, WIP

---
+++ Dialogs/BugReportDialog.cs
@@ -0,0 +1,85 @@
+﻿using EchoBot1.Services;
+using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EchoBot1.Dialogs
+{
+    public class BugReportDialog : ComponentDialog
+    {
+        private readonly BotStateService _botStateService;
+
+        public BugReportDialog(string dialogId, BotStateService botStateService) : base(dialogId)
+        {
+            _botStateService = botStateService ?? throw new ArgumentNullException(nameof(botStateService));
+
+            InitialiseWaterfallDialog();
+        }
+
+        private void InitialiseWaterfallDialog()
+        {
+            // Create waterfall steps
+            var waterfallSteps = new WaterfallStep[]
+            {
+                DescriptionStepAsync,
+                CallbackTimeStepAsync,
+                PhoneNumberStepAsync,
+                BugStepAsync,
+                SummaryStepAsync 
+            };
+
+            // Add Named Dialog
+            AddDialog(new WaterfallDialog($"{nameof(BugReportDialog)}.mainFlow", waterfallSteps)); // this is the entry point of the dialog.
+
+            AddDialog(new TextPrompt($"{nameof(BugReportDialog)}.description"));
+            AddDialog(new DateTimePrompt($"{nameof(BugReportDialog)}.callbackTime", CallbackTimeValidatorAsync));
+            AddDialog(new TextPrompt($"{nameof(BugReportDialog)}.phoneNumber", PhoneNumberValidatorAsync));
+            AddDialog(new ChoicePrompt($"{nameof(BugReportDialog)}.bug"));
+
+            // set the starting Dialog
+            InitialDialogId = $"{nameof(BugReportDialog)}.mainFlow";
+        }
+
+        private Task<bool> PhoneNumberValidatorAsync(PromptValidatorContext<string> promptContext, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task<bool> CallbackTimeValidatorAsync(PromptValidatorContext<IList<DateTimeResolution>> promptContext, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task<DialogTurnResult> SummaryStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task<DialogTurnResult> BugStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task<DialogTurnResult> PhoneNumberStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task<DialogTurnResult> CallbackTimeStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task<DialogTurnResult> DescriptionStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region Waterfall Steps
+
+        #endregion
+    }
+}
