9c8be88e2ed681e391c127fa5557cca23f79ed1e
Rex Zhiheng He <zhiheng.he@pmc.gov.au>
2019-07-15 16:36:40 +1000

    Add Greeting Dialog Class

---
+++ Dialogs/GreetingDialog.cs
@@ -0,0 +1,75 @@
+﻿using EchoBot1.Services;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EchoBot1.Dialogs
+{
+    public class GreetingDialog : ComponentDialog
+    {
+        private readonly BotStateService _botStateService;
+
+        public GreetingDialog(string dialogId, BotStateService botStateService) : base(dialogId)
+        {
+            _botStateService = botStateService ?? throw new ArgumentNullException(nameof(botStateService));
+
+            InitialiseWaterfallDialog();
+        }
+
+        private void InitialiseWaterfallDialog()
+        {
+            // Create Watefall Steps.
+            var waterfallSteps = new WaterfallStep[]
+            {
+                InitialStepAsync,
+                FinalStepAsync
+            };
+
+            // Add Named Dialogs
+            AddDialog(new WaterfallDialog($"{nameof(GreetingDialog)}.mainFlow", waterfallSteps));
+            AddDialog(new TextPrompt($"{nameof(GreetingDialog)}.name"));
+
+            // Set the starting Dialog
+            InitialDialogId = $"{nameof(GreetingDialog)}.mainFlow";
+        }
+
+        private async Task<DialogTurnResult> InitialStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var userProfile = await _botStateService.UserProfileAccessor.GetAsync(stepContext.Context, () => new Models.UserProfile());
+
+            if (string.IsNullOrEmpty(userProfile.Name))
+            {
+                return await stepContext.PromptAsync($"{(nameof(GreetingDialog))}.name",
+                    new PromptOptions
+                    {
+                        Prompt = MessageFactory.Text("What is your name")
+                    }, cancellationToken);
+            }
+            else
+            {
+                return await stepContext.NextAsync(null, cancellationToken);
+            }
+        }
+
+        private async Task<DialogTurnResult> FinalStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var userProfile = await _botStateService.UserProfileAccessor.GetAsync(stepContext.Context, () => new Models.UserProfile());
+
+            if (string.IsNullOrEmpty(userProfile.Name))
+            {
+                // Set the name
+                userProfile.Name = (string)stepContext.Result;
+                // Save any state changes that might have occured during the turn;
+                await _botStateService.UserProfileAccessor.SetAsync(stepContext.Context, userProfile);
+            }
+
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text($"Hi {userProfile.Name}. How can I help you today?"), cancellationToken);
+            return await stepContext.EndDialogAsync(null, cancellationToken);
+        }
+
+    }
+}
