11709a0672c1f27f5b1e99c2388d2d4cad990fd3
Rex Zhiheng He <zhiheng.he@pmc.gov.au>
2019-07-22 10:10:07 +1000

    Added BugReportDialog Waterfall dialog Add dialog validation

--- Dialogs/BugReportDialog.cs
+++ Dialogs/BugReportDialog.cs
@@ -1,8 +1,11 @@
 ﻿using EchoBot1.Services;
+using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Choices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -45,37 +48,109 @@ namespace EchoBot1.Dialogs
 
         private Task<bool> PhoneNumberValidatorAsync(PromptValidatorContext<string> promptContext, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var valid = false;
+
+            if (promptContext.Recognized.Succeeded)
+            {
+                valid = Regex.Match(promptContext.Recognized.Value, @"^(\+d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{4}$").Success;
+            }
+            return Task.FromResult(valid);
         }
 
         private Task<bool> CallbackTimeValidatorAsync(PromptValidatorContext<IList<DateTimeResolution>> promptContext, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var valid = false;
+
+            // This is the way we can tell the inbuilt prompt dialog validation is successful 
+            if (promptContext.Recognized.Succeeded)
+            {
+                var resolution = promptContext.Recognized.Value.First();
+
+                var selectedDate = Convert.ToDateTime(resolution.Value);
+                var start = new TimeSpan(9, 0, 0);// 10 o' clock
+                var end = new TimeSpan(17, 0, 0); // 12 o' clock
+                if ((selectedDate.TimeOfDay >= start ) && (selectedDate.TimeOfDay <= end))
+                {
+                    valid = true;
+                }
+            }
+
+            return Task.FromResult(valid);
         }
 
-        private Task<DialogTurnResult> SummaryStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        private async Task<DialogTurnResult> SummaryStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            stepContext.Values["bug"] = ((FoundChoice)stepContext.Result).Value;
+
+            // Get the current profile object from user state.
+            var userProfile = await _botStateService.UserProfileAccessor.GetAsync(stepContext.Context, () => new Models.UserProfile(), cancellationToken);
+
+            // Save all of the data inside the user profile
+            userProfile.Description = (string)stepContext.Values["description"];
+            userProfile.CallbackTime = (DateTime)stepContext.Values["callbackTime"];
+            userProfile.PhoneNumber = (string)stepContext.Values["phoneNumber"];
+            userProfile.Bug = (string)stepContext.Values["bug"];
+
+            // show the summary to the user
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text($"Here is a summary of your bug report:"), cancellationToken);
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text($"Description: {userProfile.Description}"), cancellationToken);
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text($"Callback Time: { userProfile.CallbackTime }"), cancellationToken);
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text($"Phone Number: {userProfile.PhoneNumber}"), cancellationToken);
+            await stepContext.Context.SendActivityAsync(MessageFactory.Text($"Bug: {userProfile.Bug}"), cancellationToken);
+
+            // save data in userstate
+            await _botStateService.UserProfileAccessor.SetAsync(stepContext.Context, userProfile);
+
+            // Water fall alwasy finishs with the end of the Waterfall or with another dialog, here it is the end.
+            return await stepContext.EndDialogAsync(cancellationToken);
         }
 
-        private Task<DialogTurnResult> BugStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        private async Task<DialogTurnResult> BugStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            stepContext.Values["phoneNumber"] = (string)stepContext.Result;
+
+            return await stepContext.PromptAsync($"{nameof(BugReportDialog)}.bug",
+                new PromptOptions
+                {
+                    Prompt = MessageFactory.Text("Please enter the type of bug."),
+                    Choices = ChoiceFactory.ToChoices(new List<string> { "Security", "Crash", "Power", "Performance", "Usability", "Serious Bug", "Other"})
+                }, cancellationToken);
         }
 
-        private Task<DialogTurnResult> PhoneNumberStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        private async Task<DialogTurnResult> PhoneNumberStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            stepContext.Values["callbackTimne"] = Convert.ToDateTime(((List<DateTimeResolution>)stepContext.Result).FirstOrDefault().Value);
+
+            return await stepContext.PromptAsync($"{nameof(BugReportDialog)}.phoneNumber", 
+                new PromptOptions
+                {
+                    Prompt = MessageFactory.Text("Please enter in a phone number that we can call you back at"),
+                    RetryPrompt = MessageFactory.Text("Please enter a valid phone number")
+                },
+                cancellationToken);
         }
 
-        private Task<DialogTurnResult> CallbackTimeStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        private async Task<DialogTurnResult> CallbackTimeStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            // saving the value from the stepContext, which is a place to store waterfall information in the flow.
+            // The context infomation will be removed after user exit the dialog flow 
+            stepContext.Values["description"] = (string)stepContext.Result;
+
+            return await stepContext.PromptAsync($"{nameof(BugReportDialog)}.callbackTime",
+                new PromptOptions
+                {
+                    Prompt = MessageFactory.Text("Please enter in a callback time"),
+                    RetryPrompt = MessageFactory.Text("The value entered musht be between the hours of 9 am and 5 pm")
+                }, cancellationToken);
         }
 
-        private Task<DialogTurnResult> DescriptionStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        private async Task<DialogTurnResult> DescriptionStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return await stepContext.PromptAsync($"{nameof(BugReportDialog)}.description",
+                new PromptOptions
+                {
+                    Prompt = MessageFactory.Text("Enter a description for your report")
+                }, cancellationToken);
         }
 
         #region Waterfall Steps
--- Models/UserProfile.cs
+++ Models/UserProfile.cs
@@ -8,5 +8,9 @@ namespace EchoBot1.Models
     public class UserProfile
     {
         public string Name { get; set; }
+        public string Description { get; internal set; }
+        public DateTime CallbackTime { get; internal set; }
+        public string PhoneNumber { get; internal set; }
+        public string Bug { get; internal set; }
     }
 }
